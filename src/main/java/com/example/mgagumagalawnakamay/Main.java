b04b4c82320930a2dc005983cb6457d32d0920f0
jelemiekun - pc <jeremieeden1946@gmail.com>
2023-12-19 11:55:24 +0800

    kapag dumikti sa border, mawawala current imageview then mag sspawn ng bago hahahahaha

--- src/main/java/com/example/mgagumagalawnakamay/Main.java
+++ src/main/java/com/example/mgagumagalawnakamay/Main.java
@@ -12,15 +12,14 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Scene1.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-
-        Scene1Controller scene1Controller = fxmlLoader.getController();
-        scene1Controller.initializeKamay();
-
         stage.setTitle("Mga kamay");
         stage.setResizable(false);
         stage.setFullScreen(true);
         stage.setScene(scene);
         stage.show();
+
+        Scene1Controller scene1Controller = fxmlLoader.getController();
+        scene1Controller.initializeKamay();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/mgagumagalawnakamay/Scene1Controller.java
+++ src/main/java/com/example/mgagumagalawnakamay/Scene1Controller.java
@@ -2,6 +2,7 @@ package com.example.mgagumagalawnakamay;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -10,45 +11,149 @@ import javafx.util.Duration;
 import java.util.Random;
 
 public class Scene1Controller {
-    private int ilangKamay = 100;
+    private int ilangKamay = 250;
 
+    private int directions = 8;
+    private Timeline currentTimeline;
+    private int kamayCounter = 0;
     @FXML
     private AnchorPane rootPane;
+    @FXML
+    private Label kamayCount;
 
     @FXML
     private ImageView imageView;
     @FXML
     Image image = new Image(getClass().getResourceAsStream("/com/example/mgagumagalawnakamay/Media/kamay.png"));
     private Random random = new Random();
+
     public void initializeKamay() {
         for (int i = 0; i < ilangKamay; i++) {
-            double randomX = random.nextDouble(rootPane.getPrefWidth() - 60);
-            double randomY = random.nextDouble(rootPane.getPrefHeight() - 70);
-
-            imageView = new ImageView();
-            imageView.setImage(image);
-            imageView.setLayoutX(randomX);
-            imageView.setLayoutY(randomY);
-            imageView.setFitWidth(60);
-            imageView.setFitHeight(70);
-            rootPane.getChildren().add(imageView);
-            moveKamay(imageView);
+            makeKamay();
         }
+    }
+    private void makeKamay() {
+        double randomX = random.nextDouble(rootPane.getPrefWidth() - 60);
+        double randomY = random.nextDouble(rootPane.getPrefHeight() - 70);
+
+        imageView = new ImageView();
+        imageView.setImage(image);
+        imageView.setLayoutX(randomX);
+        imageView.setLayoutY(randomY);
+        imageView.setFitWidth(60);
+        imageView.setFitHeight(70);
+        rootPane.getChildren().add(imageView);
+        moveKamay(imageView);
+        kamayCounter++;
+        displayKamayCount(kamayCounter);
+    }
+
+    private void displayKamayCount(int kamayCounters) {
+        kamayCount.setText(String.valueOf(kamayCounters));
     }
 
     private void moveKamay(ImageView imageView) {
+        int direction = random.nextInt(directions) + 1;
+
+        // Use the class-level variable to store the current timeline
+        currentTimeline = createTimeline(imageView, direction);
+
+        if (currentTimeline != null) {
+            currentTimeline.setCycleCount(Timeline.INDEFINITE);
+            currentTimeline.play();
+        }
+    }
+
+    private Timeline createTimeline(ImageView imageView, int direction) {
+        double[] delta = getDelta(direction);
+
+        double deltaY = delta[0];
+        double deltaX = delta[1];
+
+        // Add a flag to check if the ImageView has been removed
+        boolean[] removed = {false};
+
         Timeline timeline = new Timeline(
-                new KeyFrame(Duration.seconds(0.25), event -> {
-                    imageView.setLayoutY(imageView.getLayoutY() + 2);
+                new KeyFrame(Duration.seconds(0.01), event -> {
+                    imageView.setLayoutY(imageView.getLayoutY() + deltaY);
+                    imageView.setLayoutX(imageView.getLayoutX() + deltaX);
+                    if (checkBounds(imageView)) {
+                        if (!removed[0]) {
+                            rootPane.getChildren().remove(imageView);
+                            kamayCounter--;
+
+
+                            displayKamayCount(kamayCounter);
+                            if (currentTimeline != null) {
+                                makeKamay();
+                            }
+                            removed[0] = true;
+                        }
+                    } else {
+                        // Reset the removed flag when the ImageView is within bounds
+                        removed[0] = false;
+                    }
                 })
         );
-        timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.play();
+
+        return timeline;
     }
-}
 
-/*
-if (imageView.getLayoutX() == (rootPane.getPrefWidth() - 60) || imageView.getLayoutY() == (rootPane.getPrefHeight() - 70)) {
 
+    private double[] getDelta(int direction) {
+        double deltaY = 0, deltaX = 0;
+
+        // Set deltaY and deltaX based on the direction
+        switch (direction) {
+            case 1: // UP
+                deltaY = -1.3;
+                deltaX = 0;
+                break;
+            case 2: // UP-RIGHT
+                deltaY = -1.3;
+                deltaX = 1.3;
+                break;
+            case 3: // RIGHT
+                deltaY = 0;
+                deltaX = 1.3;
+                break;
+            case 4: // DOWN-RIGHT
+                deltaY = 1.3;
+                deltaX = 1.3;
+                break;
+            case 5: // DOWN
+                deltaY = 1.3;
+                deltaX = 0;
+                break;
+            case 6: // DOWN-LEFT
+                deltaY = 1.3;
+                deltaX = -1.3;
+                break;
+            case 7: // LEFT
+                deltaY = 0;
+                deltaX = -1.3;
+                break;
+            case 8: // UP-LEFT
+                deltaY = -1.3;
+                deltaX = -1.3;
+                break;
         }
- */
+
+        return new double[]{deltaY, deltaX};
+    }
+
+    private boolean checkBounds(ImageView imageView) {
+        double imageWidth = imageView.getBoundsInParent().getWidth();
+        double imageHeight = imageView.getBoundsInParent().getHeight();
+
+        double layoutX = imageView.getLayoutX();
+        double layoutY = imageView.getLayoutY();
+
+        return (layoutX + imageWidth >= rootPane.getPrefWidth() ||
+                layoutY + imageHeight >= rootPane.getPrefHeight() ||
+                layoutX <= 0 ||
+                layoutY <= 0);
+    }
+
+
+}
